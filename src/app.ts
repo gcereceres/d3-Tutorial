2636d2e6bc5447775e291163909327d8c624d676
Gus <gcereceres@gmail.com>
2018-08-17 16:55:23 -0500

    Adding first two pieces of the chart

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,25 @@
 import * as d3 from 'd3';
+
 d3.select('body');
 
-console.log('hello, world');
+const width = 960;
+const height = 480;
+
+let svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+let plotMargins = {
+    top: 30,
+    bottom: 30,
+    left: 150,
+    right: 30
+};
+
+let plotGroup = svg.append('g')
+    .classed('plot', true)
+    .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
+
+let plotWidth = width - plotMargins.left - plotMargins.right;
+let plotHeigt = height - plotMargins.top - plotMargins.bottom;
